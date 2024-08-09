39c0cd38a0c21b5c442b1e46b23ff1ef9fae3ae0
Soumya Singha Roy <Soumyasingharoy06@gmail.com>
2024-08-09 11:41:30 +0530

    Update util.py

--- util.py
+++ util.py
@@ -22,7 +22,7 @@ def set_background(image_file):
         .stApp {{
             background-image: url(data:image/png;base64,{b64_encoded});
             background-size: cover;
-            background-size: 30%;
+            background-size: 45%;
             background-repeat: no-repeat;
             background-position: center;
         }}
